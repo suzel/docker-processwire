bec8ffd13187305b50c3b9a7a1b9fbbeb9130d11
Sukru Uzel <sukru.uzel@gmail.com>
2015-03-25 17:17:24 +0200

    MYSQL_USER & MYSQL_PASS

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,21 +2,29 @@
 
 if [ -d /ProcessWire ]; then
 
-	MYSQL_PASSWORD=`pwgen -c -n -1 12`
-
 	# ProcessWire
 	rm -rf /usr/share/nginx/www
-	echo $MYSQL_PASSWORD > /ProcessWire/mysql-root-password.txt
 	mv /ProcessWire /usr/share/nginx/www
 	chown -R www-data:www-data /usr/share/nginx/www
 
 	# MySQL
+	$MYSQL_ROOT_PASS=`pwgen -c -n -1 12`
 	/usr/bin/mysqld_safe &
 	sleep 10s
 	service mysql start
-	mysqladmin -u root password $MYSQL_PASSWORD
-	mysql -uroot -p$MYSQL_PASSWORD -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_PASSWORD' WITH GRANT OPTION; FLUSH PRIVILEGES;"
-	mysql -uroot -p$MYSQL_PASSWORD -e "CREATE DATABASE processwire; GRANT ALL PRIVILEGES ON processwire.* TO 'processwire'@'localhost' IDENTIFIED BY '$MYSQL_PASSWORD'; FLUSH PRIVILEGES;"
+	mysqladmin -u root password $MYSQL_ROOT_PASS
+	mysql -uroot -p$MYSQL_ROOT_PASS -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASS' WITH GRANT OPTION; FLUSH PRIVILEGES;"
+	mysql -uroot -p$MYSQL_ROOT_PASS -e "CREATE DATABASE processwire; GRANT ALL PRIVILEGES ON processwire.* TO '$MYSQL_USER'@'localhost' IDENTIFIED BY '$MYSQL_PASS'; FLUSH PRIVILEGES;"
+
+	echo "========================================================================"
+	echo ""
+	echo "    You can now configure to this MySQL server using:"
+	echo ""
+	echo "    MySQL Root Password : $MYSQL_ROOT_PASS"
+	echo "    MySQL User : $MYSQL_USER"
+	echo "    MySQL Password : $MYSQL_PASS"
+	echo ""
+	echo "========================================================================"
 
 fi
 
